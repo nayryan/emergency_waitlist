077bd18ac763182b6d5dc025318286dcf84c40d1
Ryan <rharr080@uottawa.ca>
2024-04-08 13:40:50 -0400

    added css

---
+++ database/db_config.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost"; 
+$username = "nayryan";
+$password = "123";
+$dbname = "myDB";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Erreur de connexion: " . $e->getMessage();
+}
+?>
