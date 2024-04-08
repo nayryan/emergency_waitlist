73f51a8d8d5bf5f12fbc40e9c785ed23ade45b3f
Ryan <rharr080@uottawa.ca>
2024-04-08 00:29:10 -0400

    chnages to patient table

--- database/database.sql
+++ database/database.sql
@@ -4,4 +4,5 @@ CREATE TABLE patients(
     code VARCHAR(3) NOT NULL,
     injurySeverity TINYINT NOT NULL CHECK (injurySeverity BETWEEN 1 AND 5), -- Supposition que la gravité va de 1 à 5
     arrivalTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    UNIQUE(code) -- Assurer que le code à 3 lettres est unique pour chaque patient
 );
