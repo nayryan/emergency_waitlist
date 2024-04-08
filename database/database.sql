7ae09671c501b00c6e0bd749e126256b7ba00ea8
Ryan <rharr080@uottawa.ca>
2024-04-08 00:26:42 -0400

    database patient table

---
+++ database/database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE patients(
+    patientID INT AUTO_INCREMENT PRIMARY KEY,
+    patientName VARCHAR(200) NOT NULL,
+    code VARCHAR(3) NOT NULL,
+    injurySeverity TINYINT NOT NULL CHECK (injurySeverity BETWEEN 1 AND 5), -- Supposition que la gravité va de 1 à 5
+    arrivalTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+);
--- script.js
+++
@@ -1,9 +0,0 @@
-function refreshWaitTime() {
-    console.log("Rafraîchir le temps d'attente...");
-    document.getElementById('estimatedWait').textContent = '15 minutes';
-    document.getElementById('peopleAhead').textContent = '5 personnes';
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-    refreshWaitTime();
-});
